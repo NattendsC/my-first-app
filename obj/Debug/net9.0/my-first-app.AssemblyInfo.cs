694cec80e5f9c9776c6ce3ccdd1ef2f6407f94de
NattendsC <nathan.chilot@oteria.fr>
2025-10-09 12:39:26 +0200

    added a CI workflow for github actions

--- obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
+++ obj/Debug/net9.0/my-first-app.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c298db3a4d5c80737c3093548554dbc373bee60")]
 [assembly: System.Reflection.AssemblyProductAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyTitleAttribute("my-first-app")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
