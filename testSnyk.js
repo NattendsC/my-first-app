4ebb0fa132bbd6881d3e092e15ba10acd67fe796
NattendsC <nathan.chilot@oteria.fr>
2025-11-04 12:47:33 +0100

    Update testSnyk.js

--- testSnyk.js
+++ testSnyk.js
@@ -1,10 +1,9 @@
-// Script avec vulnérabilité SQL Injection pour tester Snyk SAST
 const express = require('express');
 const app = express();
 
 app.get('/user', (req, res) => {
   const userId = req.query.id;
-  const query = "SELECT * FROM users WHERE id = " + userId; // SQL Injection vulnerability
+  const query = "SELECT * FROM users WHERE id = " + userId; //<---- SQL Injection
   res.send(query);
 });
 
